dba7f304da5d3858e7b7c44fb584fc40161f9f05
Jimmyidds <123501633+Jimmyidds@users.noreply.github.com>
2023-03-18 11:43:17 -0400

    level 2

--- Assets/CoinScript.cs
+++ Assets/CoinScript.cs
@@ -8,6 +8,7 @@ public class CoinScript : MonoBehaviour
     public Quaternion rotation;
     public AudioSource collect;
     public AudioSource getAll;
+    public int level = 1;
 
     void Start()
     {
@@ -25,8 +26,14 @@ public class CoinScript : MonoBehaviour
             if (GameObject.Find("CoinText").GetComponent<CoinsText>().coins > 5)
             {
                 getAll.Play();
+                if (level == 1)
+                    Application.LoadLevel("Level 2");
+                if (level == 2)
+                    //win
+                    GameObject.Find("CoinText").GetComponent<CoinsText>().coins = 0;
+                level += 1;
+                gameObject.SetActive(false);
             }
-            gameObject.SetActive(false);
         }
     }
 }
